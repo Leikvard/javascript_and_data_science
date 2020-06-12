4fe56c722b4caa06b362b163e144945f30bdf428
Leikvard <wypiao@connect.hku.hk>
2020-06-12 10:42:10 +0800

    add description

--- sum_all_primes.js
+++ sum_all_primes.js
@@ -1,3 +1,4 @@
+//write sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
 function sumPrimes(num) {
     let numbers = [];
     for (var i = 2; i <= num; i++) {numbers.push(i);}
