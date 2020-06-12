411bf06e83eb98b7c0d01cd84112c55e9745cda2
Leikvard <wypiao@connect.hku.hk>
2020-06-12 10:40:15 +0800

    first edition of sum all primes

---
+++ sum_all_primes.js
@@ -0,0 +1,14 @@
+function sumPrimes(num) {
+    let numbers = [];
+    for (var i = 2; i <= num; i++) {numbers.push(i);}
+    let primes = [];
+    while (numbers[0] <= num) {
+        primes.unshift(numbers[0]);
+        numbers = numbers.filter(element => element % primes[0] !== 0);
+    }
+    return primes.reduce((sum, element) => sum + element, 0);
+  }
+  
+  var test1 = sumPrimes(10);
+  var test2 = sumPrimes(977);
+  console.log(test1, test2);
